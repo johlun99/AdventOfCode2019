9e6bd22260f17394f58b60794ce155d8fa908d16
Johan Lundgren <lundgren.johan99@gmail.com>
2019-12-01 08:40:45 +0100

    Initial commit

---
+++ AdventOfCode2019/Day01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace AdventOfCode2019
+{
+    public static class Day01
+    {
+        public static int Solution_01(int[] input)
+        {
+            int totalResult = 0;
+
+            foreach (int row in input)
+            {
+                double rowResult = Math.Floor((double)row / 3);
+                rowResult -= 2;
+
+                totalResult += (int)rowResult;
+            }
+
+            return totalResult;
+        }
+
+        public static int Solution_02(int[] input)
+        {
+            int result = 0;
+
+            foreach (int row in input)
+            {
+                double rowResult = row;
+
+                while (rowResult > 0)
+                {
+                    rowResult = Math.Floor((double)rowResult / 3);
+                    rowResult -= 2;
+
+                    if (rowResult > 0)
+                        result += (int)rowResult;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2019/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = getInput("Data/day01.txt");
+
+            int day01Res = Day01.Solution_01(input);
+
+            input = getInput("Data/day01.txt");
+            int day02Res = Day01.Solution_02(input);
+
+            Console.WriteLine(day01Res);
+            Console.WriteLine(day02Res);
+        }
+
+        private static int[] getInput(string path)
+        {
+            string[] rawInput = System.IO.File.ReadAllLines(@"../../../" + path);
+            return Array.ConvertAll(rawInput, int.Parse);
+        }
+    }
+}
